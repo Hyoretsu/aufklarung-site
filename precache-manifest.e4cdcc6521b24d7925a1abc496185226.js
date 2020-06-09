ef543b5e9ca1643d9be19c911434b1f3b81038e5
Aran Leite <hyoretsu@gmail.com>
2020-06-09 14:59:25 -0300

    Updates

---
+++ precache-manifest.e4cdcc6521b24d7925a1abc496185226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e220c154e450a290ce114cdbae08efb",
+    "url": "/aufklarung-site/index.html"
+  },
+  {
+    "revision": "b304a556bf8c94400a4c",
+    "url": "/aufklarung-site/static/js/2.fe48c37c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aufklarung-site/static/js/2.fe48c37c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf6a7d0bbb0bebe15db",
+    "url": "/aufklarung-site/static/js/main.9b48b4fa.chunk.js"
+  },
+  {
+    "revision": "703db13502561d409663",
+    "url": "/aufklarung-site/static/js/runtime-main.39f911b8.js"
+  },
+  {
+    "revision": "0a2a5837adf06580b4c68fd36ac32c8c",
+    "url": "/aufklarung-site/static/media/logo.0a2a5837.png"
+  }
+]);
